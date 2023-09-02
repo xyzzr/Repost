3128cd7700dd1f0e297f2820c0424e96742c6876
Clear <110189904+bobowawahahahbobobwahahawoaowabobwabo@users.noreply.github.com>
2023-09-02 19:28:20 +0300

    Create funny.lua

---
+++ funny.lua
@@ -0,0 +1,7 @@
+local scriptrepost = false -- script repost gets updates rarely but stays if the main script gets deleted
+
+if scriptrepost then 
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/bobowawahahahbobobwahahawoaowabobwabo/Repost/main/a"))()
+else
+  loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/dca3e69649ed196af0ac6577f743a0ae.lua"))()
+end
