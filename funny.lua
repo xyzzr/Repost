ce932cd43a150d39c8185e07c29a36132649c27f
Clear <110189904+bobowawahahahbobobwahahawoaowabobwabo@users.noreply.github.com>
2023-09-02 19:31:06 +0300

    Update funny.lua

--- funny.lua
+++ funny.lua
@@ -1,4 +1,11 @@
-local scriptrepost = false -- script repost gets updates rarely but stays if the main script gets deleted
+--[[ 
+  Key: NewThunderClientKeyFolks2023OPMEGA
+  Patched/bannable features ( DO NOT USE ) : Kill all and kill aura
+  ! If the key is invalid the reposted script is not safe to use !
+  Script repost gets updates rarely but stays if the main script gets deleted
+]]
+
+local scriptrepost = false
 
 if scriptrepost then 
   loadstring(game:HttpGet("https://raw.githubusercontent.com/bobowawahahahbobobwahahawoaowabobwabo/Repost/main/a"))()
