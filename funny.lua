1cbf0e193503242224e171984a3f2357a6452b2a
Clear <110189904+bobowawahahahbobobwahahawoaowabobwabo@users.noreply.github.com>
2023-09-02 19:34:17 +0300

    Update funny.lua

--- funny.lua
+++ funny.lua
@@ -1,14 +1,18 @@
 --[[ 
+
   Key: NewThunderClientKeyFolks2023OPMEGA
+
   Patched/bannable features ( DO NOT USE ) : Kill all and kill aura
-  ! If the key is invalid the reposted script is not safe to use !
   Script repost gets updates rarely but stays if the main script gets deleted
+
+    ! If the key is invalid the reposted script is not safe to use !
+
 ]]
 
 local scriptrepost = false
 
 if scriptrepost then 
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/bobowawahahahbobobwahahawoaowabobwabo/Repost/main/a"))()
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/bobowawahahahbobobwahahawoaowabobwabo/Repost/main/a"))()
 else
-  loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/dca3e69649ed196af0ac6577f743a0ae.lua"))()
+   loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/dca3e69649ed196af0ac6577f743a0ae.lua"))()
 end
